308047d16ef3b566f75f6dd9f6d47c1d2abb0284
Jessica <obrien.jessica6@gmail.com>
2017-07-26 01:15:38 -0400

    Add files via upload

---
+++ homework/unit_02/javaScript/js-data-modeling/hw.js
@@ -0,0 +1,164 @@
+// E-mail App
+
+var user = {
+	id: 10,
+	name: " ",
+	listOfEmailAddresses: [{
+		id: ,
+		emailAddress: 
+	}],
+}
+
+var email = {
+	id: 11,
+	emailService:[{
+		id: ,
+		name: ,
+	}]
+}
+
+
+
+
+//Radio App
+
+var radioStation = {
+	id: 12,
+	name: " ",
+	genre: " ",
+	listOfSongs: [{}];
+
+};
+
+
+var genre = {
+	id: 15,
+	name: " ",
+
+};
+
+var radioStationList = {
+	id: 20
+	genres: [{
+		id:25
+		name: " ",
+	}],
+	radioStations: [{
+		id: 25
+		name: " ",
+		genre: " ", }],
+		listOfSongs: [{
+			id: 27
+			songName: " ",
+
+		}]
+	}	
+
+
+
+// Concert App
+
+
+
+var user = {
+	id: ,
+	name: " ",
+	favBands: [{
+		id:
+		name:
+	}],
+}	
+
+
+var band = {
+	id: 30,
+	name: " ",
+	tourDate: "true",
+	ticketsOnSale: "false",
+	upComingShow: "false"
+}
+
+
+// Coffee App
+
+
+var user = {
+	id: 32,
+	name: " ",
+	listOfCoffeeNearby: [{
+		id: 
+		name: " ",
+	}],
+	shoppingCart: [{
+		menuItem:
+	}],
+	pastOrders: [{
+		menuItem:
+	}],
+	userPurchaseCoffee: true;
+}
+
+
+
+var coffeeShop = {
+	id: 38,
+	name: " ",
+	location: " ",
+	menuItem:[{
+		id: ,
+		name: ,
+	}],
+	userOrderCoffee: true;
+}
+
+
+
+
+// Sports App
+
+var user = {
+	id: 27,
+	name: " ",
+	favSportsTeams: [{
+		id: 
+		name: " ",
+	}],
+
+}
+
+
+	var sportsTeams = {
+
+		id: 34,
+		name: " ",
+		teamStats: [{
+			rank:
+			titles:
+		}],
+		players: [{
+			id: ,
+			name: ,
+			playerStats: ,
+		}],
+		mostCurrentStat: "false",
+	}
+
+
+
+
+
+	// I thought about how I would like to use the app.
+
+
+
+
+
+	
+
+
+
+
+
+
+	}]
+}
