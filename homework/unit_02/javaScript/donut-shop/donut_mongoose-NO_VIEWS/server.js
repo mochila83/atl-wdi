067acd28bde8896a8c13cf928c238cd993a2d265
Jessica <obrien.jessica6@gmail.com>
2017-07-27 00:47:10 -0400

    Add files via upload

---
+++ homework/unit_02/javaScript/donut-shop/donut_mongoose-NO_VIEWS/server.js
@@ -0,0 +1,90 @@
+//======================
+// REQUIREMENTS
+//======================
+// require express, mongoose, body-parser, method-override
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var hbs = require("hbs");
+var logger = require('morgan');
+
+
+//======================
+// MIDDLEWARE
+//======================
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+app.set("view engine", "hbs");
+app.set('views', './views');
+
+app.use(express.static(__dirname + 'public'));
+app.use( logger('dev'));
+
+//======================
+// CONTROLLERS
+//======================
+//for seed file, seed the database
+var seedController = require('./controllers/seeds.js');
+app.use('/seed', seedController);
+
+//for root directory, show all donuts
+var donutsController = require('./controllers/donuts.js');
+app.use('/', donutsController);
+
+//======================
+// LISTENERS
+//======================
+ var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/donut_store');
+
+var db = mongoose.connection;
+
+
+db.on('error', function(err){
+  console.log(err);
+});
+//CREATE THE MONGOOSE CONNECTION and SET APP TO LISTEN to 3000
+
+db.once('open', function() {
+  console.log("database has been connected!");
+});
+
+app.listen(3000, function(){
+  console.log("app listening on port 3000");
+});
+
+
+
+
+
+//CREATE THE MONGOOSE CONNECTION and SET APP TO LISTEN to 3000
+
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/donut_store');
+
+var db = mongoose.connection;
+
+
+db.on('error', function(err){
+  console.log(err);
+});
+//CREATE THE MONGOOSE CONNECTION and SET APP TO LISTEN to 3000
+
+db.once('open', function() {
+  console.log("database has been connected!");
+});
+
+app.listen(3000, function(){
+  console.log("app listening on port 3000");
+});
+
+
+
+
+
