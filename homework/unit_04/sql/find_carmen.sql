70bfcf719cb76bfc5ed38766bc895089e4dd3599
Jessica <obrien.jessica6@gmail.com>
2017-08-18 00:02:41 -0400

    Add files via upload

---
+++ homework/unit_04/sql/find_carmen.sql
@@ -0,0 +1,66 @@
+
+1.
+--carmen=# SELECT name FROM country WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
+--             name
+-- -------------------------------
+ -- Holy See (Vatican City State)
+-- (1 row)
+
+2. 
+--  language
+-- ----------
+-- Italian
+-- (1 row)
+
+
+3. 
+
+--  SMR         | Italian  | t          |        100
+-- ITA         | Italian  | t          |       94.1
+
+
+--    name
+-- ------------
+--  San Marino
+-- (1 row)
+
+
+4. 
+
+--     name
+-- ------------
+ -- Serravalle
+ -- San Marino
+-- (2 rows)
+
+
+  --  id  |    name    | countrycode |     district      | population
+ -- ------+------------+-------------+-------------------+------------
+ --  265 | Serra      | BRA         | Espï¿½rito Santo  |     302666
+ -- 3170 | Serravalle | SMR   
+ --       | Serravalle/Dogano |       4802
+
+5. 
+
+--  name
+ -- --------
+-- Brazil
+-- (1 row)
+
+
+6.
+
+--    name
+-- ------------
+-- Bras�lia
+-- (1 row)
+
+
+7. 
+
+ --    name     |  district  | countrycode
+-- --------------+------------+-------------
+-- Santa Monica | California | USA
+-- (1 row)
+
+
