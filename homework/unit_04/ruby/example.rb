d302c189798c426e9f1849f8640e5e75b322259a
Jessica <obrien.jessica6@gmail.com>
2017-08-22 23:48:34 -0400

    Add files via upload

---
+++ homework/unit_04/ruby/example.rb
@@ -0,0 +1,50 @@
+# 1
+a = true
+b = false
+
+puts a == b
+puts a || b
+
+# 2
+n = nil
+
+p n 
+
+# 3
+d = 7
+puts d 
+
+e = 4.4
+
+puts e 
+
+d = e
+
+puts d
+
+# 4
+jess = "Hi, I'm Jess"
+
+likes_to = "hiking in the woods"
+
+sentence = "#{jess} and I like #{likes_to}"
+
+puts sentence 
+
+g = 2
+
+g_to_number = 2
+
+puts g + g_to_number
+
+# 5
+puts "What's your name?"
+name = gets.chomp
+puts "What do you like?"
+like = gets.chomp.downcase    
+puts "I am #{name} and I like #{like}"  
+
+
+
+
+
